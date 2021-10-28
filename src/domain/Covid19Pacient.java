71767083ccc45252a1f13aa4c3af3a12aa971a3e
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2021-10-28 10:57:27 +0200

    commit

---
+++ src/domain/Covid19Pacient.java
@@ -0,0 +1,98 @@
+package domain;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Observable;
+import java.util.Set;
+
+import factory.SymptonFactory;
+import iterator.Covid19PacientIterator;
+
+public class Covid19Pacient {
+	private String  name; 
+	private int age;
+	private Map<Symptom,Integer> symptoms=new HashMap<Symptom,Integer>();
+
+	public Covid19Pacient(String name, int years) {
+		this.name = name;
+		this.age = years;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getWeight(Symptom s) {
+		return symptoms.get(s);
+	}
+
+	public Set<Symptom> getSymptoms() {
+		return symptoms.keySet();
+	}
+	public Iterator<Symptom> iterator(){
+		return new Covid19PacientIterator(new ArrayList<Symptom>(symptoms.keySet()));
+	}
+	
+	public Symptom getSymptomByName(String symptomName) {
+		Iterator<Symptom> i= getSymptoms().iterator();
+		Symptom s=null;
+		while (i.hasNext()) {
+			s=i.next();
+			if (s!=null && s.getName().equals(symptomName)) return s;
+		}
+		return null;	
+	}
+
+	public void addSymptom(Symptom c, Integer w){
+		symptoms.put(c,w);
+	}
+	
+	public Symptom addSymptomByName(String symptom, Integer w){
+		Symptom s=getSymptomByName(symptom);
+		if (s==null) {
+			s=new SymptonFactory().createSymptom(symptom); 
+			symptoms.put(s,w);		
+		}
+		return s;
+	}
+
+	public Symptom removeSymptomByName(String symptomName) {
+		Symptom s=getSymptomByName(symptomName);
+		System.out.println("Simptom to remove: "+s);
+		if (s!=null) symptoms.remove(s);
+		return s;
+	}
+	
+	public double covidImpact() {
+		double afection=0;
+		double increment=0;
+		double impact=0;
+		
+		//calculate afection
+		for (Symptom c: symptoms.keySet()) {
+			if (c!=null )
+			   afection=afection+c.getSeverityIndex()*symptoms.get(c);
+		}
+		afection=afection/symptoms.size();
+		
+		//calculate increment
+		if (getAge()>65) increment=afection*0.5;
+		
+		//calculate impact
+		impact=afection+increment;
+		return impact;
+	}
+	
+}
+
